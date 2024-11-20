3253ce70aeacff4e68f8918e019be3d2d47fa2bc
Martin <martin_lenart@icloud.com>
2024-11-20 17:51:08 +0900

    initial commit

---
+++ M3_Yahtzee/CupDoubleDice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Seido.Utilities.SeedGenerator;
+
+namespace M3_Yahtzee
+{
+    /*
+    class CupDoubleDice : CupOfDice, ICupDoubleDice
+    {
+    }
+    */
+}
